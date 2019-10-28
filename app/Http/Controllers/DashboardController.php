e9b70608b20215f4e2714bb702d4c7b66319cc95
Luis García <luis_grdz@hotmail.com>
2019-10-28 07:45:10 -0600

    Avance: Uso de Request con Laravel y dd como función para debuggear las variables como con el uso de var_dump y die

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     //Author: Luis Alberto Gracía Rodríguez
-    public function index() {
-        return view('welcome', ['title'=>'Curso En Platzi']);
+    public function index(Request $request) {
+        // dd() funciona como: var_dump($variable); die;
+        // dd($request->query('title', 'Valor por default'));
+        // return view('welcome', ['title'=>'Curso En Platzi']);
+        return view('welcome', [
+            'title'=> $request->query('title', 'Valor por default')
+            ] 
+        );
     }
 }
